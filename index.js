dc1fa5dc0b1071db03598353b0b08434528c2f30
Adnan-Eram-Argho <adnaneramargho@gmail.com>
2024-06-04 15:40:23 +0600

    jwt done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors')
 const port = 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require("jsonwebtoken");
 
 app.use(cors());
 app.use(express.json())
@@ -13,6 +14,26 @@ app.use(express.json())
 
 
 const uri = process.env.DATABASE_URL;
+
+
+function createToken(user){
+    const token = jwt.sign({
+        email:user.email
+      }, 'secret', { expiresIn: '7d' });
+
+      return token;
+}
+function verifyToken(req, res, next) {
+    const token = req.headers.authorization.split(" ")[1];
+    const verify = jwt.verify(token, "secret");
+    console.log(verify)
+    if (!verify?.email) {
+      return res.send("You are not authorized");
+    }
+    req.user = verify.email;
+    next();
+  }
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,7 +54,7 @@ async function run() {
         const userCollection = userDB.collection("userCollection");
 
         //news Article routes
-        app.post('/news_articles', async (req, res) => {
+        app.post('/news_articles', verifyToken, async (req, res) => {
             const newsData = req.body;
             const result = await news_articles_collection.insertOne(newsData)
             res.send(result)
@@ -48,7 +69,7 @@ async function run() {
             const newsData = await news_articles_collection.findOne({ _id: new ObjectId(id) })
             res.send(newsData)
         })
-        app.patch('/news_articles/:id', async (req, res) => {
+        app.patch('/news_articles/:id', verifyToken,async (req, res) => {
             const id = req.params.id;
             const updateData = req.body;
             const newsData = await news_articles_collection.updateOne(
@@ -57,7 +78,7 @@ async function run() {
             )
             res.send(newsData)
         })
-        app.delete('/news_articles/:id', async (req, res) => {
+        app.delete('/news_articles/:id',verifyToken, async (req, res) => {
             const id = req.params.id;
             console.log(id)
             const newsData = await news_articles_collection.deleteOne({ _id: new ObjectId(id) })
@@ -76,16 +97,18 @@ async function run() {
         //user
         app.post('/user', async (req, res) => {
             const user = req.body;
+            const token=createToken(user)
+            console.log(token)
             const isUserExist = await userCollection.findOne({ email: user?.email });
             if (isUserExist?._id) {
                 return res.send({
                     statu: "success",
                     message: "Login success",
-                    //   token,
+                      token,
                 });
             }
-            const result = await userCollection.insertOne(user);
-            res.send(result)
+             await userCollection.insertOne(user);
+            res.send({token})
         })
 
         app.get("/user/get/:id", async (req, res) => {
@@ -101,7 +124,7 @@ async function run() {
             res.send(result);
         });
 
-        app.patch("/user/:email", async (req, res) => {
+        app.patch("/user/:email", verifyToken, async (req, res) => {
             const email = req.params.email;
             const userData = req.body;
             const result = await userCollection.updateOne(
